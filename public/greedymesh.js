5af898cfd4a5a027797f4378bf0cec4d71e54fd2
John <john.grindall@gmail.com>
2017-06-05 17:22:01 +0100

    corrections

--- public/greedymesh.js
+++ public/greedymesh.js
@@ -1,25 +1,36 @@
 
 
-var MAX = 24;
+var SIZE_I = 8;
+var SIZE_J = 8;
 
 
 
 var makeEmpty = function(){
     var a = [];
-    for(var _i = 0; _i < MAX; _i++){
+    for(var _i = 0; _i < SIZE_I; _i++){
         a[_i] = [];
-        for(var _j = 0; _j < MAX; _j++){
+        for(var _j = 0; _j < SIZE_J; _j++){
             a[_i][_j] = 0;
         }
     }
     return a;
 };
 
+var log = function(a){
+	for(var _i = 0; _i < SIZE_I; _i++){
+		var s = "";
+        for(var _j = 0; _j < SIZE_J; _j++){
+			s += a[_i][_j] + " ";
+        }
+		console.log(s);
+    }
+};
+
 var makeRnd = function(){
     var a = [];
-    for(var _i = 0; _i < MAX; _i++){
+    for(var _i = 0; _i < SIZE_I; _i++){
         a[_i] = [];
-        for(var _j = 0; _j < MAX; _j++){
+        for(var _j = 0; _j < SIZE_J; _j++){
             a[_i][_j] = (Math.random() < 0.5) ? 0 : 1;
         }
     }
@@ -28,29 +39,53 @@ var makeRnd = function(){
 
 var img = makeRnd();
 
+//img = [[1,0,1],[0,0,1],[1,1,1]];
+
+log(img);
+
 var greedymesh = function(a){
-    var i = 0, j = 0, quads = [];
+    var i = 0, j = -1, quads = [];
     var visited = makeEmpty();
+	var isInside = function(){
+		return (i < SIZE_I && j < SIZE_J && i >= 0 && j >= 0);
+	};
     var moveToNext = function(){
-        var ok = function(){
-            return ( i<= MAX && j <= MAX && !visited[i][j] && a[i][j]);
-        };
-        var step = function(){
+		//console.log("I start at", i, j);
+		var shouldVisitCell = function(){
+			return (isInside() && a[i][j] && !visited[i][j]);
+		};
+		var step = function(){
             j++;
-            if(j > MAX){
+            if(j >= SIZE_J){
                 j = 0;
                 i++;
+				if(i >= SIZE_I){
+					throw new Error("out of bounds");
+				}
             }
+			//console.log("I stepped to", i, j);
         };
-        while(!ok() && i <= MAX && j <= MAX){
-            step();
-        }
-        return (i <= MAX && j <= MAX);
+		if(shouldVisitCell()){
+			return;
+		}
+		try{
+			while(!shouldVisitCell()){
+				step();
+			}
+		}
+		catch(e){
+			// outside
+			return false;
+		}
+		//console.log("ended up at", i, j);
+        return isInside();
     };
     var rectIsFullAndNotVisited = function(i, j, w, h){
+		//console.log("check rect is full and not vis", i, j, "  ", w, h);
         for(var _i = i; _i < i + h; _i++){
             for(var _j = j; _j < j + w; _j++){
                 if(!a[_i][_j] || visited[_i][_j]){
+					// no, break!
                     return false;
                 }
             }
@@ -66,22 +101,27 @@ var greedymesh = function(a){
     };
     var buildQuad = function(){
         var w = 1, h = 1;
-        while(j + w <= MAX + 1 && rectIsFullAndNotVisited(i, j, w, h) ){
+		//console.log("build at", i, j);
+        while(w <= SIZE_J - j && rectIsFullAndNotVisited(i, j, w, h) ){
             w++;
         }
         w--;
-        while(i + h <= MAX + 1 && rectIsFullAndNotVisited(i, j, w, h) ){
+		//console.log("got w", w);
+        while(h <= SIZE_I - i && rectIsFullAndNotVisited(i, j, w, h) ){
             h++;
         }
         h--;
-        console.log('found', i, j, ', ', w, h);
+		//console.log("got h", h);
+        //console.log('found!!  ', i, j, ', ', w, h);
         mark(i, j, w, h);
         quads.push([i, j, w, h]);
     };
     while(moveToNext()){
         buildQuad();
     }
-    console.log('quads', quads);
+    console.log('quads', JSON.stringify(quads, null, 2));
+	return quads;
 };
 
-greedymesh(img);
+
+var quads = greedymesh(img);
