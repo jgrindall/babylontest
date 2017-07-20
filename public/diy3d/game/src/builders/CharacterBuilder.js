d80eacfea7881d32ac85594e9b1a96d30b73c6ea
John <john.grindall@gmail.com>
2017-07-20 14:52:33 +0100

    simplify

--- public/diy3d/game/src/builders/CharacterBuilder.js
+++ public/diy3d/game/src/builders/CharacterBuilder.js
@@ -9,9 +9,9 @@ define(["diy3d/game/src/utils/GridUtils", "diy3d/game/src/cache/AnimationCache"]
 	};
 
 	CharacterBuilder.addBaddie = function(pos, scene, meshCache, manager, id, obj, grid, playerPos){
-		var y = SIZE/2, mesh, babylonPos, texture;
-		var meshComp = manager.getComponentDataForEntity('MeshComponent', id);
-		var strategyComp = manager.getComponentDataForEntity('BaddieStrategyComponent', id);
+		var y = SIZE/2, mesh, meshComp, strategyComp, babylonPos, texture;
+		meshComp = manager.getComponentDataForEntity('MeshComponent', id);
+		strategyComp = manager.getComponentDataForEntity('BaddieStrategyComponent', id);
 		babylonPos = GridUtils.ijToBabylon(pos[0], pos[1]);
 		mesh = meshCache.getBaddie(scene, obj.data.texture);
 		mesh.position = new BABYLON.Vector3(babylonPos.x, y, babylonPos.z);
@@ -21,15 +21,15 @@ define(["diy3d/game/src/utils/GridUtils", "diy3d/game/src/cache/AnimationCache"]
 	};
 
 	CharacterBuilder.addPlayer = function(pos, scene){
-		var y = SIZE/2;
-		var babylonPos = GridUtils.ijToBabylon(pos[0], pos[1]);
-		var player = BABYLON.MeshBuilder.CreateBox("player", {height: SIZE*0.75, width:SIZE*0.75, depth:SIZE*0.75}, scene);
-		player.checkCollisions = true;
-		player.setEnabled(false);
-		player.isVisible = false;
-		player.position = new BABYLON.Vector3(babylonPos.x, y, babylonPos.z);
-		player.ellipsoid = new BABYLON.Vector3(SIZE/4, SIZE/4, SIZE/4);
-		return player;
+		var y = SIZE/2, babylonPos, mesh;
+		babylonPos = GridUtils.ijToBabylon(pos[0], pos[1]);
+		mesh = BABYLON.MeshBuilder.CreateBox("player", {height: SIZE*0.75, width:SIZE*0.75, depth:SIZE*0.75}, scene);
+		mesh.checkCollisions = true;
+		mesh.setEnabled(false);
+		mesh.isVisible = false;
+		mesh.position = new BABYLON.Vector3(babylonPos.x, y, babylonPos.z);
+		mesh.ellipsoid = new BABYLON.Vector3(SIZE/4, SIZE/4, SIZE/4);
+		return mesh;
 	};
 
 	return CharacterBuilder;
