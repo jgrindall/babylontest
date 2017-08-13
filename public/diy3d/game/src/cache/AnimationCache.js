73ec97702b1b15780a8ed940a75e9a3d9ec25961
John <john.grindall@gmail.com>
2017-08-13 10:37:07 +0100

    NUM_FRAMES added

--- public/diy3d/game/src/cache/AnimationCache.js
+++ public/diy3d/game/src/cache/AnimationCache.js
@@ -6,17 +6,21 @@ define([], function(){
 
 	var AnimationCache = {};
 
+	var NUM_FRAMES = 10; // even please
+
+	//no performance gain changing these - just change them to get the right speed
+
 	AnimationCache.get = function(type){
 		if(type === "rot"){
-			var a = new BABYLON.Animation("rot", "rotation.z", 10, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+			var a = new BABYLON.Animation("rot", "rotation.z", NUM_FRAMES, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
 			a.setKeys([{
 			    frame: 0,
 			    value: 2
 			},{
-			    frame: 10,
+			    frame: NUM_FRAMES/2,
 			    value: -2
 			},{
-			    frame: 2 * 10,
+			    frame: NUM_FRAMES,
 			    value: 2
 			}]);
 			return a;
